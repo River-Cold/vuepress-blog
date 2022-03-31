415964c95b0b8c093e37e6902a3a756d0faf46c0
River-Cold <rivercoldjh@163.com>
2022-03-31 22:28:39 +0800

    deploy

--- docs/sidebar.js
+++ docs/sidebar.js
@@ -53,7 +53,7 @@ module.exports = [
                         title: '数据库', 
                         prefix: 'database/',
                         path: 'database/',
-                        children: ["/", "数据库基础知识", "MySQL重要知识点"],
+                        children: ["/", "数据库基础知识", "MySQL重要知识点", "MySQL知识点&面试题总结"],
                     },
                     {
                         title: '计算机网络', 
